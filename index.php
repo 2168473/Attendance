be86dbaa605c990a465c2028943f95493977565e
Albert Baterina <2147582@gmail.com>
2018-06-14 11:36:32 +0800

    Improved HomePage
    Also added eventViewer.html

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ date_default_timezone_set('Asia/Manila');
                                                 <img src='data:image;base64," . base64_encode($event[3]) . "'>
                                              </div>
                                                 <p>$event[1]</p>
-                                                <a href=''>Read More</a>
+                                                <a href='pagefragments/eventViewer.html'>Read More</a>
                                         </div>
                                     ";
                         }
@@ -96,188 +96,35 @@ date_default_timezone_set('Asia/Manila');
 
                 <div class="ui bottom attached tab segment active" data-tab="loginButton">
                     <div class="ui compact container">
-
-                        <div class="ui horizontal divider">
-                            <h3>Login Here</h3>
-                        </div>
-
-
-                        <form class="ui form very padded" id="login-form" method="post">
-                            <div class="ui segment">
-                                <div class="field">
-                                    <div class="ui left icon input">
-                                        <i class="user icon"></i>
-                                        <input type="text" name="email" placeholder="E-mail address" id="ea">
-                                    </div>
-                                </div>
-                                <div class="field">
-                                    <div class="ui left icon input">
-                                        <i class="icon lock"></i>
-                                        <input type="password" name="password" placeholder="Password">
-                                    </div>
-                                </div>
-                                <div class="field">
-                                    <div class="ui selection dropdown left icon">
-                                        <input type="hidden" name="purpose">
-                                        <i class="id badge outline icon"></i>
-                                        <div class="default text grey">Purpose</div>
-                                        <i class="dropdown icon"></i>
-                                        <div class="menu">
-                                            <div class="item" data-value="Regular Member">Regular Member</div>
-                                            <div class="item" data-value="Drop-in Coworking">Drop-in Coworking</div>
-                                            <div class="item" data-value="Attend Event">Attend Event</div>
-                                            <div class="item" data-value="Guest/Other">Guest/Other</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="ui fluid large submit button">Login</div>
-                            </div>
-
-                            <div class="ui error message"></div>
-                        </form>
-
-                        <div class="ui message center aligned">New to us?
-                            <button class="mini ui button signup" id="signupButton">Sign Up</button>
-                        </div>
+                        <?php
+                            include 'pagefragments/login.html';
+                        ?>
                     </div>
                 </div>
 
                 <!-- Logout Form -->
                 <div class="ui bottom attached tab segment" data-tab="logoutButton">
                     <div class="ui compact container">
-                        <div class="ui horizontal divider">
-                            <h3>Enter E-mail to logout</h3>
-                        </div>
-                        <form class="ui form very padded">
-                            <div class="ui segment">
-                                <div class="field">
-                                    <div class="ui left icon input">
-                                        <i class="user icon"></i>
-                                        <input type="text" name="email" placeholder="E-mail address">
-                                    </div>
-                                </div>
-                                <div class="ui fluid large submit button">Logout</div>
-                            </div>
-
-                            <div class="ui error message"></div>
-                        </form>
+                        <?php
+                            include 'pagefragments/logout.html';
+                        ?>
                     </div>
                 </div>
 
                 <!-- Registration Form -->
                 <div class="ui bottom attached tab segment" data-tab="signupButton">
                     <div class="ui compact container">
-                        <div class="ui horizontal divider">
-                            <h3>Registration</h3>
-                        </div>
-                        <form class="ui compact form">
-                            <div class="ui segment">
-                                <div class="equal width fields">
-                                    <div class="field">
-                                        <div class="ui left icon input">
-
-                                            <i class="user icon"></i>
-                                            <input placeholder="First Name" type="text">
-                                        </div>
-                                    </div>
-                                    <div class="field">
-                                        <div class="ui left icon input">
-
-                                            <i class="user icon"></i>
-                                            <input placeholder="Last Name" type="text">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="equal width fields">
-                                    <div class="field">
-                                        <div class="ui left icon input">
-                                            <i class="envelope outline icon"></i>
-                                            <input placeholder="user@email.com" type="email">
-                                        </div>
-                                    </div>
-                                    <div class="field">
-                                        <div class="ui labeled input">
-                                            <div class="ui label">+63</div>
-                                            <input placeholder="Enter mobile number" type="text">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="equal width fields">
-                                    <div class="field">
-                                        <div class="ui left icon input">
-                                            <i class="lock icon"></i>
-                                            <input placeholder="Enter password" type="password">
-                                        </div>
-                                    </div>
-                                    <div class="field">
-                                        <div class="ui left icon input">
-                                            <i class="lock icon"></i>
-                                            <input placeholder="Confirm password" type="password">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="field">
-                                    <div class="fluid field">
-                                        <div class="ui left icon input">
-                                            <i class="building outline icon"></i>
-                                            <input placeholder="Company/Organization" type="text">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="ui fluid large submit button">Sign-up</div>
-                            </div>
-
-                            <div class="ui error message"></div>
-                        </form>
+                        <?php
+                            include 'pagefragments/registration.html';
+                        ?>
                     </div>
                 </div>
 
                 <div class="ui bottom attached tab segment" data-tab="inquireButton">
                     <div class="ui compact container">
-                        <div class="ui horizontal divider">
-                            <h3>Inquiry</h3>
-                        </div>
-                        <form class="ui compact form">
-                            <div class="ui segment">
-                                <div class="equal width fields">
-                                    <div class="field">
-                                        <div class="ui left icon input">
-                                            <i class="user icon"></i>
-                                            <input placeholder="Full Name" type="text" name="name">
-                                        </div>
-                                    </div>
-                                    <div class="field">
-                                        <div class="ui left icon input"><i class="envelope outline icon"></i>
-                                            <input placeholder="user@email.com" type="email" name="email">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="equal width fields">
-                                    <div class="field">
-                                        <div class="ui left icon input">
-                                            <i class="envelope icon"></i>
-                                            <input placeholder="Subject Matter" type="text" name="subject">
-                                        </div>
-                                    </div>
-                                    <div class="field">
-                                        <div class="ui labeled input">
-                                            <div class="ui label">+63</div>
-                                            <input placeholder="Enter mobile number" type="text" name="mobile">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="equal width fields">
-                                    <div class="field">
-                                        <textarea rows="3" placeholder="Enter message here..."
-                                                  name="message"></textarea>
-                                    </div>
-
-                                </div>
-                                <div class="ui fluid large submit button">Submit</div>
-                            </div>
-                            <div class="ui error message"></div>
-                        </form>
+                        <?php
+                            include 'pagefragments/inquire.html';
+                        ?>
                     </div>
                 </div>
             </div> <!-- End of right section (Form Area) -->
